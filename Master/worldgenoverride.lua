2e64a95650decf04a69e9f8d2752728b972165b7
KemuriAme <kemuriame4@gmail.com>
2019-12-16 21:59:47 +0000

    default world test.

---
+++ Master/worldgenoverride.lua
@@ -0,0 +1,7 @@
+return {
+    override_enabled = true,
+    preset = "DST_MAIN",
+    misc = {
+    world_size = "huge",
+	}
+}
---
+++ startDST.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+steamcmd_dir="$HOME/steamcmd"
+install_dir="$HOME/DSTserver"
+cluster_name="Documents"
+cd "$install_dir/bin"
+
+run_shared=(./dontstarve_dedicated_server_nullrenderer)
+run_shared+=(-console)
+run_shared+=(-cluster "DST_MAIN")
+run_shared+=(-monitor_parent_process $$)
+
+"${run_shared[@]}" -shard Master | sed 's/^/Master: /' &
+"${run_shared[@]}" -shard Caves  | sed 's/^/Caves: /'
